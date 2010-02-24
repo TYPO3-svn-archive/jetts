f9974f6f939e72a8acba2f50a0417c3c9da3e6db
ilomedia <ilomedia@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-24 09:08:30 +0000

    New hook to add extended permissions on content elements : For example users are allowed to insert a text element on column 0 but not a bullet list (which could be allowed on column 1). Edition, deletion or the ability to hide an element can also be fine tuned thanks to this hook Permissions can be set globally for pagetree, by column er even by content uid.
    example configuration : 
    
    plugin.tx_jetts.acl {
    	byContentType {
    		noEdit = bullets
    		noHide = textpic
    		noDelete = image
    		allowed = text,textpic,image,bullets,form
    	}
    	byUid {
    		noEdit = 2
    		noHide = 21
    		noDelete = 8
    	}
    	byColumn {
    		0 {
    			noEdit = image
    			noHide = text
    			noDelete = bullets,text
    			allowed = text,textpic,image
    		}
    	}
    }
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jetts/trunk@30418 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,4 +4,7 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi/class.tx_jetts_parser.php','_parser','',1);
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi/class.tx_jetts_selector.php','_selector','',1);
 
+$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:jetts/hooks/class.tx_jetts_tcemainprocdm.php:tx_jetts_tcemainprocdm';
+$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = 'EXT:jetts/hooks/class.tx_jetts_tcemainprocdm.php:tx_jetts_tcemainprocdm';
+
 ?>
